496bab4256415be878ead95dd98578b216ca231c
nielshulstaert <nielshulstaert@gmail.com>
2021-05-10 11:17:58 +0200

    oglycans first commit

---
+++ src/main/java/com/compomics/oglycans/FastaReader.java
@@ -0,0 +1,13 @@
+package com.compomics.oglycans;
+
+import com.compomics.util.experiment.massspectrometry.SpectrumFactory;
+
+import java.io.File;
+
+public class FastaReader {
+
+    public void readFasta(File fastaFile){
+        Fasta
+    }
+
+}
